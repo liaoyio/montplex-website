6beb4a1754b2bb82add9cc2c7d5c04c0002cec68
liaoyi <2417276459@qq.com>
2023-06-28 14:46:34 +0800

    Feat: website SSG.

---
+++ src/init.ts
@@ -0,0 +1,10 @@
+import Cookies from "js-cookie";
+
+export function handleConsole() {
+	const cookie = Cookies.get("Auth-Token");
+	if (!cookie) {
+		window.open("https://dev.montplex.com/engula/auth0/login");
+	} else {
+		window.open("https://dev.montplex.com/console");
+	}
+}
